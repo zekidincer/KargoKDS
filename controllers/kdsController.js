c39ff34437ec75485b8c3565af1ae3176f2eca10
zekidincer <zekide14@gmail.com>
2025-12-31 19:04:56 +0300

    Update kdsController.js

--- controllers/kdsController.js
+++ controllers/kdsController.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 
-// --- VERİTABANI BAĞLANTISI  ---
+// --- VERİTABANI BAĞLANTISI   ---
 const db = mysql.createConnection({
     host: 'localhost', 
     user: 'root', 
